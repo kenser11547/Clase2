4a8b3cdf48bf89f78346eceb3999d5e25d5b084c
kenser <koffy140221@gmail.com>
2023-05-04 12:05:37 -0600

    se agrero ña funcion para la info y para ruedas

--- herenciaypoliformismo.php
+++ herenciaypoliformismo.php
@@ -3,5 +3,15 @@
         protected int $ruedas;
         protected int $capacidad;
         public function __construct(int $ruedas, int $capacidad)
-        
+        {
+            $this->ruedas = $ruedas;
+            $this->capacidad = $capacidad;
+        }
+        public function getInfo() : string {
+            return "El transporte tiene " . $this->ruedas . "ruedas y una capacidad de " . $this->capacidad . " personas ";
+        }
+
+        public function getRuedas() : int {
+            return $this->ruedas;
+        }
     }
