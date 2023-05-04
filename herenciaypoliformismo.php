34a972d28a3871704027e01a098a7d09f837c210
kenser <koffy140221@gmail.com>
2023-05-04 12:10:34 -0600

    agregar la class bicicleta de transporte

--- herenciaypoliformismo.php
+++ herenciaypoliformismo.php
@@ -15,3 +15,13 @@
             return $this->ruedas;
         }
     }
+    class Bicicleta extends Transporte {
+        public function getInfo() : string{
+            return "el transporte tiene " . $this->ruedas . "ruedas y una capacidad de" . $this->capacidad . "personas y no gasta gasolina"
+        }
+    }
+
+    class Automovil extends Transporte {
+        protected string $transmision;
+        public function __construct(int $ruedas, int $capacidad, string $transmision)
+    }
